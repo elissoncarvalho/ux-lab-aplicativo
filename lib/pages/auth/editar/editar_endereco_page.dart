47a3ebdf71671d04a46d34a2c5a2171bcdc0f6ab
Wesley Junio <wesleyjunio323@gmail.com>
2019-11-22 21:55:50 -0300

    Correção

---
+++ lib/pages/auth/editar/editar_endereco_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:uxlab/widgets/ux_input.dart';
+
+class EditarEnderecoPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+        title: Text(
+          'Editar Endereço',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.blueGrey,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.close,
+            color: Colors.blueGrey,
+          ),
+          onPressed: () => Navigator.pop(context, false),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.done),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: Colors.blueGrey,
+          ),
+        ],
+        ),
+        backgroundColor: Colors.white,
+         body: ListView(
+            children: [
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 70, right: 30, left: 30),
+                  child: Column(
+                    children: [
+                      UxInput(
+                        textLabel: 'Novo Endereço',
+                        textColor: Colors.blue,
+                        labelColor: Colors.blue,
+                        padding: const EdgeInsets.only(bottom: 50),
+                      ),
+                      UxInput(
+                        textLabel: 'Novo Número',
+                        textColor: Colors.blue,
+                        labelColor: Colors.blue,
+                        padding: const EdgeInsets.only(bottom: 50),
+                        keyboardType: TextInputType.number,
+                      ),
+                       UxInput(
+                        textLabel: 'Novo Bairro',
+                        keyboardType: TextInputType.text,
+                        textColor: Colors.blue,
+                        labelColor: Colors.blue,
+                        padding: const EdgeInsets.only(bottom: 50),
+                      ),
+                       UxInput(
+                        textLabel: 'Novo CEP',
+                        textMask: MaskedTextController(mask: '00000-000'),
+                       keyboardType: TextInputType.number,
+                        textColor: Colors.blue,
+                        labelColor: Colors.blue,
+                        padding: const EdgeInsets.only(bottom: 50),
+                      ),
+                      ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+    );
+  }
+}
